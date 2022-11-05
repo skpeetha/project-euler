79e49f51b18bab0b8d68242a696f1d6edf6b64be
MopideviAward <78560350+MopideviAward@users.noreply.github.com>
2022-11-05 16:52:06 +0530

    update generate prime numbers

--- python/generatePrimeNumbers.py
+++ python/generatePrimeNumbers.py
@@ -1,26 +1,30 @@
 
 limitN = int(input("Enter numerical limit value:")) # fails if enetred value is not a number
 print(f"Entered value is {limitN}")
-
 i = 5
 primes = [2,3]
-tempN = limitN
-print(tempN)
 
-while i <= limitN:
-    #for p in primes:
-    #    if i%p == 0:
-    #        break
+
+def generate(num):
+    global i
+    global primes
+
+    while i <= num:
+        for p in primes:
+            if i%p == 0:
+                break
+            elif p == primes[-1]:
+                primes = primes + [i]
+    return(primes)
     #primes = primes + [i]
-    j = 2
-    while j < i:
-        if i%j == 0:
-            break
-        j = j + 1
-    else:
-        primes = primes + [i]
-    i = i + 1
-    
-
+    #j = 2
+    #while j < i:
+    #    if i%j == 0:
+    #        break
+    #    j = j + 1
+    #else:
+    #    primes = primes + [i]
+        i = i + 2
+generate(limitN)
 print(f'Prime list: {primes}')
 
