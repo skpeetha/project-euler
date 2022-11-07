215d6c5da8ffdb051d7dbd7f73a0ba3302d1faef
MopideviAward <78560350+MopideviAward@users.noreply.github.com>
2022-11-07 12:59:14 +0530

    problem 4

--- python/problem4.py
+++ python/problem4.py
@@ -1,17 +1,29 @@
 # biggest palindrome when 2 n digit numbers are multiplied
 
-limitN = input("Enter numerical limit value:")
-print(f"Entered value is {limitN}")
 
-length = len(limitN)
-print(f'length:{length}')
-i= 0
-while i > length/2:
-     if limitN[0] != limitN[-1-i]:
-         print(f'not palindrome')
-         exit
-     i = i+1
-     print(f'in loop')
-     
+def check_palindrome(num):
+    if num[0] == num[-1]:
+        i = 1
+        length = len(num)
+        #print(f'length:{length}:{num}')
+        while i < length//2:
+            if num[i] != num[-1-i]:
+                return False
+            i = i+1
+        return True
+    else:
+        return False
 
-print(f'Sum of values is ')
+i = 100
+pal = 0
+while i < 1000:
+    j = 100
+    while j < 1000:
+        prod = i*j
+        if check_palindrome(str(prod)) & (prod > pal):
+            pal = prod
+            print(f'pal:{pal}')
+        j = j + 1
+    i  = i + 1
+
+print(f'MAx palindrome {pal}')
