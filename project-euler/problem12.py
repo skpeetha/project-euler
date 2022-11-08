5fc9dda4dc7320c8d222ca38e4c0172f5245f1fa
Srikanth Peetha <srikanth.peetha@gmail.com>
2022-11-08 14:08:20 +0530

    problem 12

---
+++ project-euler/problem12.py
@@ -0,0 +1,15 @@
+# tringluar numbers
+i = 2
+current_tn = 3
+while True:
+	count = 1
+	for f in range(1,int(current_tn/2)+1):
+		if current_tn%f == 0:
+			count = count + 1
+			#print(f'i:{i} tn:{current_tn} factor:{f}')
+			if count > 500:
+				print(f'iteration:{i}\ntriangular number:{current_tn}')
+				quit()
+	i = i + 1
+	#print(i)
+	current_tn = i*(i+1)/2
