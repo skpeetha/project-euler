c93f958d63c346bd1b34374feebf339f103d6bca
Srikanth Peetha <srikanth.peetha@gmail.com>
2022-11-24 21:06:10 +0530

    template update

--- project-euler/template.py
+++ project-euler/template.py
@@ -1,5 +1,9 @@
+import time
+from math_lib import generate_primes_range
+
+start_time = time.time()
 
 limitN = int(input("Enter numerical limit value:")) # fails if enetred value is not a number
 print(f"Entered value is {limitN}")
 
-
+print(f'Execution duration : {start_time - time.time()}')
