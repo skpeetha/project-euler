cc1013c1265df30354c83b6c24ac4ec2e5480d3a
Srikanth Peetha <srikanth.peetha@gmail.com>
2022-11-24 21:00:00 +0530

    p12 incomplete

--- project-euler/problem12.py
+++ project-euler/problem12.py
@@ -9,6 +9,16 @@ n = 6 #sum of first i digits
 primes = [2]
 startR = 3
 
+
+def m_list(num,f_list):
+    f_set = set()
+    for f in f_list:
+        f_set.add(num*f)
+    return f_list[0]*num,f_set
+
+#print(m_list(4,[2,3,3,5]))
+#quit()
+
 if n < startR:
     print(f'min start values should be 2')
     quit()
@@ -17,33 +27,43 @@ while True:
     endR = int(n/2 + 1)
     primes = primes + generate_primes_range(startR,endR)
     #2 3 5 7 11 13
-    factors = {1,n}
+    factors = [] #prime factors of n
+    di = {1} # divisors of n
     for p in primes:
         res = n
         count = 0
         bflag = False
         while res%p == 0:
             res = int(res/p)
-            count = count + 1
-            factors.add(p**count)
-            factors.add(res)
+            di.add(p)
+            di.add(res)
+            factors.append(p)
             if res in primes:
+                factors.append(res)
                 bflag = True
                 break
         if bflag:
             break
-    if len(factors) > 500:
-        print(f'Number : {n}')
-        print(f'Execution duration : {start_time - time.time()}')
+
+    #print(f'factors {n}: {factors}')
+    for it,p in enumerate(factors):
+        temp_lt =  factors[it:]
+        prd = p
+        while len(temp_lt) > 1:
+            prd, temp_set = m_list(prd,temp_lt[1:])
+            di = di.union(temp_set)
+            temp_lt = temp_lt[1:]
+
+    if len(di) > 500:
+        print(f'Number : {factors}')
+        print(f'iterator: {i} divisors of {n}: -- {len(di)}')
+        print(f'Execution duration : {time.time() - start_time}')
         quit()
-    print(f'iterator: {i} divisors of {n}: {factors} -- {len(factors)}')
+    print(f'iterator: {i} divisors of {n}: -- {len(di)}')
     startR = endR
     i = i + 1
     n = int(i*(i+1)/2)
     
-
-# iterator: 406 divisors of 82621: {1, 2849, 37, 7, 11, 29, 7511, 2233, 11803, 82621} -- 10
-# actual 16 :1, 7, 11, 29, 37, 77, 203, 259, 319, 407, 1073, 2233, 2849, 7511, 11803, 82621
 
 
 
