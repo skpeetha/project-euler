c665d639feebccf18ff73e3fd9cc901c61ba66a7
Srikanth Peetha <srikanth.peetha@gmail.com>
2022-11-25 18:33:03 +0530

    p16 complete

--- project-euler/problem16.py
+++ project-euler/problem16.py
@@ -3,6 +3,11 @@ from math_lib import generate_primes_range
 
 start_time = time.time()
 
-
+res = 2**1000
+sum_n = 0
+while res//10 != 0:
+    sum_n = sum_n + res%10
+    res = res//10
+print(sum_n+res)
 
 print(f'Execution duration : {time.time() - start_time}')
