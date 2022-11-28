7bcaf39ae7cc9a033a927b02274b4c94a4cdf874
Srikanth Peetha <srikanth.peetha@gmail.com>
2022-11-28 23:45:25 +0530

    p23 complete

--- project-euler/problem23.py
+++ project-euler/problem23.py
@@ -1,3 +1,4 @@
+#very inefficient method, took 1987 sec to complete
 import time
 from math_lib import generate_primes_range
 
@@ -11,10 +12,25 @@ def get_divisors_sum(n):
     return sum_d
 
 n = 12
+abd_n = []
+nu = list(range(1,28123))
 
-
-
-while n < 28123
+while n < 28123:
+    if get_divisors_sum(n) > n:
+        abd_n += [n]
+        print(n)
+    n += 1
+len_abd = len(abd_n)
+for ind,abd in enumerate(abd_n):
+    #nu.remove(2*abd)
+    i = 0
+    while i < len(abd_n):
+        if abd_n[i]+abd in nu:
+            nu.remove(abd_n[i]+abd)
+            
+        i += 1
+    print(f'{len_abd}:{ind}:{len(nu)}')
+print(sum(nu))
 
 
 print(f'Execution duration : {time.time() - start_time}')
